a41ed640ce09d23421e18203273b71e4a535ac0a
tulio-limaa <tulio.lima310@gmail.com>
2025-04-08 21:35:17 -0400

    Add files via upload

--- models/movie.js
+++ models/movie.js
@@ -1,1 +1,27 @@
+// Import mongoose to work with MongoDB
+import mongoose from 'mongoose';
 
+// Define the movie schema
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  director: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  releaseYear: {
+    type: Number,
+  },
+  genre: {
+    type: String,
+    enum: ['Action', 'Romance', 'Drama', 'Horror', 'Thriller', 'Other']
+  },
+}, { timestamps: true });
+
+//Create a movie model
+const Movie = mongoose.model('Movie', movieSchema);
+export default Movie;
